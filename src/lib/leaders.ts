b7efd8716f2332ea75e779e8aa7d613291947457
jusobreira <jugsobreira@gmail.com>
2026-05-07 14:18:31 -0300

    Update leaders.ts

--- src/lib/leaders.ts
+++ src/lib/leaders.ts
@@ -146,10 +146,10 @@ export const LEADERS: Leader[] = [
   { id: "p-117", code: "P-117", name: "Nami", image: "https://limitlesstcg.nyc3.cdn.digitaloceanspaces.com/one-piece/P/P-117_EN.webp", color: "#FF6B35" },
   { id: "others", code: "others-otros", name: "Outros", image: "https://static.wixstatic.com/media/858f16_188302edd0ba41b085ce526a30c792fe~mv2.png", color: "#FF6B35" },
  // Paises
-  { id: "País-Brasil", code: "País-Brasil", name: "Brasil", image: "https://www.shutterstock.com/image-illustration/brazil-flag-silk-3d-illustration-260nw-447141448.jpg", color: "#9D4EDD" },
+  { id: "País-Brasil", code: "País-Brasil", name: "Brasil", image: "https://t3.ftcdn.net/jpg/01/63/82/20/360_F_163822042_fh2VMaxEJKHpyVF3tjm4pOMdr5ZJiaUE.jpg", color: "#9D4EDD" },
   { id: "País-EUA", code: "País-EUA", name: "EUA", image: "https://www.eccobandeiras.com.br/image/cache/catalog/antigas/Estados-Unidos-1111x740.jpg", color: "#9D4EDD" },
   { id: "País-Chile", code: "País-Chile", name: "Chile", image: "https://www.flagsonline.it/uploads/2016-6-6/1200-0/chile.jpg", color: "#9D4EDD" },
-  { id: "País-Peru", code: "País-Peru", name: "PERU", image: "https://www.eccobandeiras.com.br/image/cache/catalog/antigas/Estados-Unidos-1111x740.jpg", color: "#9D4EDD" },
+  { id: "País-Peru", code: "País-Peru", name: "PERU", image: "https://flagsusa.com/cdn/shop/files/products-perugovtPS__99476.1514879360.1280.1280.jpg", color: "#9D4EDD" },
   { id: "País-México", code: "País-México", name: "México", image: "https://www.federalflags.com/cdn/shop/products/0012_Mexico-flag-emblem_6a001ebf-b9e1-4dba-85ea-1aacfe478397.jpg", color: "#9D4EDD" },
   { id: "País-Bolívia", code: "País-Bolívia", name: "Bolívia", image: "https://t3.ftcdn.net/jpg/03/36/04/60/360_F_336046029_3htJ8DoV4FGBrNSM7ehEZcRtlWdCBqR4.jpg", color: "#9D4EDD" },
   { id: "País-Equador", code: "País-Equador", name: "Equador", image: "https://img.freepik.com/fotos-premium/bandeira-do-equador-balancando-ao-vento_2227-1992.jpg", color: "#9D4EDD" },
